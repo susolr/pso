cd3dd480ce5d6a7473770e9ee38c0f126dbdb710
susolr <jlopezpeque@gmail.com>
2022-02-21 19:34:28 +0100

    Class Paramlist

--- src/paramlist.cpp
+++ src/paramlist.cpp
@@ -37,13 +37,13 @@ Paramlist::Paramlist(int argc, char* argv[]){
 }
 
 void Paramlist::init(){
-    lista.push_back(Parametro("-nP", "Parámetro para definir el número de párticulas que conforman el cúmulo"));
-    lista.push_back(Parametro("-nH", "Parámetro para definir el número de hebras en paralelo"));
-    lista.push_back(Parametro("-nI", "Parámetro para definir el número máximo de iteraciones del bucle principal"));
-    lista.push_back(Parametro("-cS", "Parámetro para definir el valor de la componente social del algoritmo"));
-    lista.push_back(Parametro("-cC", "Parámetro para definir el valor de la componente cognitiva del algoritmo"));
-    lista.push_back(Parametro("-cI", "Parámetro para definir el valor de la componente de inercia del algoritmo"));
-    lista.push_back(Parametro("-k" , "Parámetro para definir el valor K del algoritmo de clasificación"));
+    lista.push_back(Parametro("-nP", "Parámetro para definir el número de párticulas que conforman el cúmulo", "NParticulas"));
+    lista.push_back(Parametro("-nH", "Parámetro para definir el número de hebras en paralelo", "NHebras"));
+    lista.push_back(Parametro("-nI", "Parámetro para definir el número máximo de iteraciones del bucle principal", "NMaxIteraciones"));
+    lista.push_back(Parametro("-cS", "Parámetro para definir el valor de la componente social del algoritmo", "CSocial"));
+    lista.push_back(Parametro("-cC", "Parámetro para definir el valor de la componente cognitiva del algoritmo", "CCognitiva"));
+    lista.push_back(Parametro("-cI", "Parámetro para definir el valor de la componente de inercia del algoritmo", "CInercia"));
+    lista.push_back(Parametro("-k" , "Parámetro para definir el valor K del algoritmo de clasificación", "KValor"));
 }
 
 int Paramlist::buscarParametro(string arg){
