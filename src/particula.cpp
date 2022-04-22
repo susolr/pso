0886700ee18841cf6a4a54cfb7b5d00f57b6dd5a
susolr <jlopezpeque@gmail.com>
2022-04-22 10:17:51 +0200

    Fin de pruebas

--- src/particula.cpp
+++ src/particula.cpp
@@ -109,9 +109,9 @@ Particula::Particula(int n){
     n_hebras = stoi((Paramlist::getInstance()->getValor("-nH")));
     max_iter = stoi((Paramlist::getInstance()->getValor("-nI")));
     for (int i = 0; i < dimension; i++){
-        //double v = rand()%7 - 3;
-        //vel.push_back(v);
-        vel.push_back(0.0);
+        double v = rand()%7 - 3;
+        vel.push_back(v);
+        //vel.push_back(0.0);
         
     }
     //vel = generarAleatorio();
