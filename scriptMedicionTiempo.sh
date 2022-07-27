4b3d4c78cfc5c6d8794eeb6ae9135af64aaee0b5
Jesus Lopez <jesuslr1999@correo.ugr.es>
2022-07-28 01:30:24 +0200

    Medicion

--- scriptMedicionTiempo.sh
+++ scriptMedicionTiempo.sh
@@ -2,22 +2,23 @@
 ################# Global variables
 NREPETITIONS=5
 NMAXHEBRAS=12
-DIR="mediciones_tiempo_k_3"
+NMAXNODOS=4
+DIR="mediciones_tiempo_multi"
 rm -rf $DIR && mkdir $DIR
 date && echo "Start the execution"
 echo "-------------------"
 make -j N_FEATURES=3600
-for H in $( eval echo {1..$NMAXHEBRAS} );
+for N in $( eval echo {1..$NMAXNODOS} );
 do
     echo -e "\t`date`" && echo -e "\tStarts with" $H "hebras"
-    DIR+="/$H hebras"
+    DIR+="/$N nodos"
     mkdir "$DIR"
     for R in $( eval echo {1..$NREPETITIONS} );
     do
         echo -e "\t`date`" && echo -e "\tStarts " $R "repetition"
         echo "Repetition: $R" >> "$DIR/raw.txt"
         echo -e "\t" $R
-        salloc -N1 -n1 -p guest -w compute-0-4 mpiexec --bind-to none -x OMP_NUM_THREADS=$H ./pso > last.txt
+        salloc -N$N -n1 -p guest mpiexec --bind-to none -x OMP_NUM_THREADS=12 ./pso > last.txt
         #mpiexec ./pso -nH $H > last.txt
         echo "`sed -n 1p last.txt`" >> tmp_time.txt
     done
