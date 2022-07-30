0418699ac0b8cd5a9a16184690ce78f7fe76eadc
Jesus Lopez <jesuslr1999@correo.ugr.es>
2022-07-30 02:12:29 +0200

    Hybrid

--- src/pso.cpp
+++ src/pso.cpp
@@ -116,6 +116,25 @@ void PSO::ejecutar(){
     //cout << "Proceso: " << stoi(Paramlist::getInstance()->getValor("MPIrank")) <<" Antes de barrier" << endl << flush;
     MPI::COMM_WORLD.Barrier();
     //cout << "Proceso: " << stoi(Paramlist::getInstance()->getValor("MPIrank")) <<" despues de barrier" << endl << flush;
+
+                int numprocs, rank, namelen;
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int iam = 0, np = 1;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &numprocs);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Get_processor_name(processor_name, &namelen);
+
+    // omp_set_num_threads(omp_get_max_threads());
+    #pragma omp parallel default(shared) private(iam, np)
+    {
+        np = omp_get_num_threads();
+        iam = omp_get_thread_num();
+        #pragma omp critical
+        {
+            printf("Hybrid: Hello from thread %d/%d from process %d/%d on %s\n", iam, np, rank, numprocs, processor_name);
+        }
+    }
     
     while (contador < n_max_iter){
         cout << "Proceso: " << stoi(Paramlist::getInstance()->getValor("MPIrank")) <<" entra a valorar" << endl << flush;
