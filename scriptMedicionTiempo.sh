bde218b3f6a099a583e638dd05bbe55289afd066
Jesus Lopez <jesuslr1999@correo.ugr.es>
2022-08-01 01:22:23 +0200

    Mediciones MPI

--- scriptMedicionTiempo.sh
+++ scriptMedicionTiempo.sh
@@ -2,14 +2,14 @@
 ################# Global variables
 NREPETITIONS=5
 NMAXHEBRAS=12
-NMAXNODOS=6
+NMAXNODOS=7
 NODES="compute-0-2,compute-0-3,compute-0-4"
 DIR="mediciones_tiempo_multi"
 rm -rf $DIR && mkdir $DIR
 date && echo "Start the execution"
 echo "-------------------"
 make -j N_FEATURES=3600
-for N in $( eval echo {6..$NMAXNODOS} );
+for N in $( eval echo {7..$NMAXNODOS} );
 do
     echo -e "\t`date`" && echo -e "\tStarts with" $N "hebras"
     DIR+="/$N nodos"
@@ -19,7 +19,7 @@ do
         echo -e "\t`date`" && echo -e "\tStarts " $R "repetition"
         echo "Repetition: $R" >> "$DIR/raw.txt"
         echo -e "\t" $R
-        salloc -p guest -N$N -n$N -w compute-0-4,compute-0-1,compute-0-2,compute-0-3,compute-0-6,compute-0-7 mpiexec --map-by node --bind-to none -x OMP_NUM_THREADS=12 ./pso > last.txt
+        salloc -p guest -N$N -n$N -w compute-0-4,compute-0-1,compute-0-2,compute-0-3,compute-0-6,compute-0-7,compute-0-8 mpiexec --map-by node --bind-to none -x OMP_NUM_THREADS=12 ./pso > last.txt
         #mpiexec ./pso -nH $H > last.txt
         echo "`sed -n 1p last.txt`" >> tmp_time.txt
     done
