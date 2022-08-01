b8663f773a188a8909f808bac633acb63e715a1c
Jesus Lopez <jesuslr1999@correo.ugr.es>
2022-08-01 17:11:14 +0200

    Particulas

--- src/pso.cpp
+++ src/pso.cpp
@@ -205,7 +205,8 @@ void PSO::ejecutar(){
         //cout << "Velocidad y posicion actualizadas" << endl;
         //var_value = abs(b_value-aux_value);
         clas_media = clas_media/cumulo.size();
-        cout << "0," << clas_media << "\t0," << b_value << "\t" << b_k << endl;
+        //cout << "0," << clas_media << "\t0," << b_value << "\t" << b_k << endl;
+        cout << "0," << clas_media << "\t0," << b_value << endl;
         contador++;
         
     }
