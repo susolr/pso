fe30b56fe700bddba794f325950deb8ebf3c1d08
Jesus Lopez <jesuslr1999@correo.ugr.es>
2022-08-11 19:09:05 +0200

    Coeficiente Social

--- scriptParametros.sh
+++ scriptParametros.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 ################# Global variables
 NREPETITIONS=5
-VC=(0.2 0.4 0.6 0.8 1.0)
+VS=(0.2 0.4 0.6 0.8 1.0)
 NMAXHEBRAS=12
 NMAXNODOS=7
 NODES="compute-0-2,compute-0-3,compute-0-4"
-DIR="coeficiente_cognitivo"
+DIR="coeficiente_social"
 rm -rf $DIR && mkdir $DIR
 date && echo "Start the execution"
 echo "-------------------"
 make -j N_FEATURES=3600
-for C in "${VC[@]}";
+for S in "${VS[@]}";
 do
     echo -e "\t`date`" && echo -e "\tStarts with" $C "cognitivo"
-    DIR+="/$C cognitivo"
+    DIR+="/$S social"
     mkdir "$DIR"
     for R in $( eval echo {1..$NREPETITIONS} );
     do
         echo -e "\t`date`" && echo -e "\tStarts " $R "repetition"
         echo "Repetition: $R" >> "$DIR/raw.txt"
         echo -e "\t" $R
-        salloc -p guest -N3 -n3 -w compute-0-1,compute-0-2,compute-0-7 mpiexec --map-by node --bind-to none -x OMP_NUM_THREADS=12 ./pso -cC $C > last.txt
+        salloc -p guest -N3 -n3 -w compute-0-1,compute-0-2,compute-0-7 mpiexec --map-by node --bind-to none -x OMP_NUM_THREADS=12 ./pso -cS $S > last.txt
         #mpiexec ./pso -nH $H > last.txt
         #echo "`sed -n 1p last.txt`" >> tmp_time.txt
         paste last* > "$DIR/clasificacion_$R.txt"
